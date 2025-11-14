d088ec794027c6a7c04e0543aa4ec28892c32c2f
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-11-14 07:51:58 -0500

    Cambios

---
+++ pregunta_1/src/pregunta_1.scala
@@ -0,0 +1,6 @@
+object Estadistica:
+
+  def myMethod(datos: List[Double]): Double =
+    val promedio = datos.sum / datos.size
+    val sumaCuadrados = datos.map(x => math.pow(x - promedio, 2)).sum
+    math.sqrt(sumaCuadrados / datos.size)
